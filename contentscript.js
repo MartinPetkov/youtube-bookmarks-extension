596cffb8cd567b5eacd2388f30bb29e8afe13f79
Martin Petkov <martin.petkov@mail.utoronto.ca>
2016-01-23 08:21:51 -0500

    Slightly reworked version, would allow storing data using the chrome APIs now

--- contentscript.js
+++ contentscript.js
@@ -13,3 +13,59 @@ j.onload = function() {
 };
 
 (document.head || document.documentElement).appendChild(j);
+
+
+window.addEventListener("message", function(event) {
+  // We only accept messages from ourselves
+  if (event.source != window)
+    return;
+
+  if (event.data.type && (event.data.type == "FROM_PAGE")) {
+    console.log("Content script received: " + event.data);
+
+    // TODO: Save these in Chrome storage
+    var current_time = event.data.current_time;
+    var description = event.data.description;
+    console.log(current_time, description);
+
+  }
+}, false);
+
+function addBookmark() {
+  window.postMessage({type: "FROM_CONTENT_SCRIPT", add_current_bookmark: true}, "*");
+}
+
+
+window.onload = function () {
+  console.log('Hello World!');
+
+  yt = document.getElementById('movie_player');
+
+  // Add the sidebar
+  var sidebar = $(document.createElement('div'));
+  sidebar.addClass('youtube-bookmarks-sidebar wordwrap');
+
+  var add_bookmark_button = $(document.createElement('button'));
+  add_bookmark_button.addClass('yt-uix-button yt-uix-button-default youtube-bookmarks-button');
+  add_bookmark_button.prop('value', 'Add Bookmark');
+  add_bookmark_button.text('Add Bookmark');
+  add_bookmark_button.click(addBookmark);
+  add_bookmark_button.removeAttr('onhover');
+  sidebar.append(add_bookmark_button);
+
+  var description = $('<input type="text" name="youtube-bookmark-description" id="youtube-bookmark-description" placeholder="Description"></input>');
+  description.addClass('youtube-bookmark-description');
+  sidebar.append(description);
+
+  var bookmarks_title = $(document.createElement('div'));
+  bookmarks_title.addClass('youtube-bookmarks-title');
+  bookmarks_title.text('Bookmarks');
+  sidebar.append(bookmarks_title);
+
+  var bookmarks_list = $(document.createElement('ul'));
+  bookmarks_list.attr('id', 'youtube-bookmarks-list');
+  sidebar.append(bookmarks_list);
+
+  sidebar.insertBefore(yt);
+}
+
--- youtube-bookmarks.js
+++ youtube-bookmarks.js
@@ -1,3 +1,7 @@
+function getCurrentVideoTime() {
+  return document.getElementById('movie_player').getCurrentTime();
+}
+
 function makeStrTime(sec_num) {
     var hours   = Math.floor(sec_num / 3600);
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
@@ -32,80 +36,54 @@ function getSeconds(sec_num) {
   return Math.floor(sec_num - (getHours(sec_num) * 3600) - (getMinutes(sec_num) * 60));
 }
 
-function getCurrentVideoTime() {
-  return document.getElementById('movie_player').getCurrentTime();
-}
-
-
 function makeSeekTo(h, m, s){
-  return 'yt.seekTo(('+h.toString()+'*60+'+m.toString()+')*60+'+s.toString()+');return false;';
+  return 'document.getElementById("movie_player").seekTo(('+h.toString()+'*60+'+m.toString()+')*60+'+s.toString()+');return false;';
 }
-
-
-function addBookmark() {
-  var sec_num = getCurrentVideoTime();
-  var description = $('#youtube-bookmark-description').val();
-  $('#youtube-bookmark-description').val('');
-
-  var a = $(document.createElement('a'));
-  a.addClass('youtube-bookmark');
-  a.attr('href', '#');
-  a.attr('onclick', makeSeekTo(getHours(sec_num), getMinutes(sec_num), getSeconds(sec_num)));
-  a.text(makeStrTime(sec_num))
-
-  var a_remove_btn = $('<button type="button" class="yt-uix-button yt-uix-button-default a-remove-btn" aria-label="Remove"><bold>X<bold></button>');
-  a_remove_btn.click(removeBookmark);
-
-  var li = $(document.createElement('li'));
-  li.addClass('youtube-bookmark-li');
-  li.append(a_remove_btn);
-  li.append(a);
-
-  if(description) {
-    var desc = $(document.createElement('span'));
-    desc.text(': ' + description);
-    li.append(desc);
-  }
-
-  $('#youtube-bookmarks-list').append(li);
-}
-
 
 function removeBookmark() {
   $(this).parent().remove();
 }
 
 
+window.addEventListener("message", function(event) {
+  // We only accept messages from ourselves
+  if (event.source != window)
+    return;
 
-window.onload = function () {
-  console.log('Hello World!');
+  if (event.data.type && (event.data.type == "FROM_CONTENT_SCRIPT")) {
+    console.log("Page received: " + event.data);
 
-  yt = document.getElementById('movie_player');
+    if(event.data.add_current_bookmark) {
+      var sec_num = getCurrentVideoTime();
+      var description = $('#youtube-bookmark-description').val();
+      $('#youtube-bookmark-description').val('');
 
-  // Add the sidebar
-  var sidebar = $(document.createElement('div'));
-  sidebar.addClass('youtube-bookmarks-sidebar wordwrap');
+      var a = $(document.createElement('a'));
+      a.addClass('youtube-bookmark');
+      a.attr('href', '#');
+      a.attr('onclick', makeSeekTo(getHours(sec_num), getMinutes(sec_num), getSeconds(sec_num)));
+      a.text(makeStrTime(sec_num))
 
-  var add_bookmark_button = $(document.createElement('button'));
-  add_bookmark_button.addClass('yt-uix-button yt-uix-button-default youtube-bookmarks-button');
-  add_bookmark_button.prop('value', 'Add Bookmark');
-  add_bookmark_button.text('Add Bookmark');
-  add_bookmark_button.click(addBookmark);
-  add_bookmark_button.removeAttr('onhover');
-  sidebar.append(add_bookmark_button);
+      var a_remove_btn = $('<button type="button" class="yt-uix-button yt-uix-button-default a-remove-btn" aria-label="Remove"><bold>X<bold></button>');
+      a_remove_btn.click(removeBookmark);
 
-  var description = $('<input type="text" name="youtube-bookmark-description" id="youtube-bookmark-description" placeholder="Description"></input>');
-  description.addClass('youtube-bookmark-description');
-  sidebar.append(description);
+      var li = $(document.createElement('li'));
+      li.addClass('youtube-bookmark-li');
+      li.append(a_remove_btn);
+      li.append(a);
 
-  var bookmarks_title = $(document.createElement('div'));
-  bookmarks_title.addClass('youtube-bookmarks-title');
-  bookmarks_title.text('Bookmarks');
-  sidebar.append(bookmarks_title);
+      if(description) {
+        var desc = $(document.createElement('span'));
+        desc.text(': ' + description);
+        li.append(desc);
+      }
 
-  var bookmarks_list = $(document.createElement('ul'));
-  bookmarks_list.attr('id', 'youtube-bookmarks-list');
-  sidebar.append(bookmarks_list);
+      $('#youtube-bookmarks-list').append(li);
 
-  sidebar.insertBefore(yt);
-}
+      window.postMessage({type: "FROM_PAGE", current_time: sec_num, description: description}, "*");
+
+    } else if (event.data.add_bookmark) {
+      console.log(event.data);
+    }
+  }
+}, false);
